7623a96ffa475273d4e8cb41e1286e9d9fb64357
Elle Holbrook <elle.holbrook@gmail.com>
2017-11-07 20:50:54 -0700

    working on cleaning description, fix functions so they actually work

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -1,84 +1,91 @@
+library(stats)
+library(broom)
+library(dplyr)
+
 # function to create a 95% confidence interval
-perc_cis <- function(x, n) {
+perc_cis <- function(x = x, n = n){
+  library(scales) 
   
-  prob <- (x / n)
-  SE <- sqrt((prob*(1-prob)/n))
-  CI_lower <- prob - (1.96 * (SE*prob))
-  CI_upper <- prob + (1.96 * (SE*prob))
+  proportion <-  x/n
+  est_of_stand <-  sqrt((proportion*(1 - proportion))/n)
+  low_CI <- proportion-(1.96*est_of_stand)
+  upp_CI <- proportion+(1.96*est_of_stand)
   
-  CI_lower_perc <- round(CI_lower*100, digits = 1)
-  CI_upper_perc <- round(CI_upper*100, digits = 1)
-  prob_perc <- round(prob*100, digits = 1)
-  print_out <- paste0(prob_perc, "%", " (", CI_lower_perc, "%",", ", CI_upper_perc, "%", ")")
-  print_out
+  prop <- percent(proportion)
+  low_conf <- percent(low_CI)
+  upp_conf <- percent(upp_CI)
+  
+  
+  perc_cis_vec <- paste(prop, " ", "(", low_conf, ",", " ",upp_conf, ")", sep = "")
+  return(perc_cis_vec)
 }
 
 
 #function for testing for trend using Cochran-Armitage trend test
-test_trend_ca <- function(drug, df = clean_fars){
-  if(drug == "Nonalcohol") {
-    
-    nonalcohol <- df %>% 
-      mutate(alchyesno = drug_type != "Alcohol") %>% 
-      filter(alchyesno == "TRUE") %>%
+test_trend_ca <- function(drug, data = clean_fars) {
+  if(drug == "Nonalcohol"){
+    clean <- clean_fars %>%
+      filter(drug_type != "Alcohol") %>%
       group_by(unique_id, year) %>% 
-      summarize(positive_for_drug = any(positive_for_drug, na.rm = TRUE)) %>% 
+      summarize(positive_test = sum(positive_for_drug, na.rm = TRUE), 
+                positive = any(positive_test > 0),
+                total_tests = length(!is.na(positive_for_drug))) %>% 
       ungroup() %>% 
-      mutate(drug_type = "Nonalcohol") %>%
       group_by(year) %>%
-      summarize(positive = sum(positive_for_drug, na.rm = TRUE),
-                trials = sum(!is.na(positive_for_drug)))
-    ca_alcohol <- prop.trend.test(x = nonalcohol$positive,
-                                  n = nonalcohol$trials)
-    ca_alcohol <- data.frame(Z = sqrt(ca_alcohol$statistic), p.value = ca_alcohol$p.value)
-    row.names(ca_alcohol) <- NULL
-    as_tibble(ca_alcohol)  
-  }
-  else {
-    to_test <- df %>%
+      summarize(total_tests = sum(total_tests), positive = sum(positive)) 
+  } else{
+    clean <- clean_fars %>%
       filter(drug_type == drug) %>%
       group_by(year) %>%
       summarize(positive = sum(positive_for_drug, na.rm = TRUE),
-                trials = sum(!is.na(positive_for_drug)))
-    ca_alcohol <- prop.trend.test(x = to_test$positive,
-                                  n = to_test$trials)
-    
-    ca_alcohol <- data.frame(Z = sqrt(ca_alcohol$statistic), p.value = ca_alcohol$p.value)
-    row.names(ca_alcohol) <- NULL
-    as_tibble(ca_alcohol)
+                total_tests = sum(!is.na(positive_for_drug)))
   }
+  ca_drug_type <- prop.trend.test(x = clean$positive,
+                                  n = clean$total_tests)
+  Z <-  sqrt(ca_drug_type$statistic)
+  p.value <- ca_drug_type$p.value
+  
+  Z <- round(Z, digits = 1)
+  p.value <- round(p.value, digits = 3)
+  
+  test_trend_table <- tibble::tibble(Z, p.value)
+  
+  test_trend <- test_trend_table %>% 
+    mutate(Z = as.numeric(Z)) %>% 
+    mutate(p.value = as.numeric(p.value))
+  
+  return(test_trend)
 }
 
 
 #function for testing for trend using logistic regression
-test_trend_log_reg <- function(drug, df = clean_fars){
-  if(drug == "Nonalcohol") {
-    
-    nonalcohol <- df %>% 
-      mutate(alchyesno = drug_type != "Alcohol") %>% 
-      filter(alchyesno == "TRUE") %>%
+test_trend_log_reg <- function(drug, data = clean_fars) {
+  if(drug == "Nonalcohol"){
+    to_test <- clean_fars %>%
+      filter(!is.na(drug_type)) %>% 
+      filter(drug_type != "Alcohol") %>% 
       group_by(unique_id, year) %>% 
-      summarize(positive_for_drug = any(positive_for_drug, na.rm = TRUE)) %>% 
-      ungroup() %>% 
-      mutate(drug_type = "Nonalcohol")
-    log_reg <- glm(positive_for_drug ~ year, data = nonalcohol,
-                   family = binomial(link = "logit"))
-    x <- summary(log_reg)$coefficients
-    x = as.data.frame(x)
-    x %>% 
-      slice(2) %>% 
-      select(3:4)    
+      summarize(positive_for_drug = any(positive_for_drug))
     
-  }
-  else {
-    alcohol <- df %>%
+  } else{
+    to_test <- clean_fars %>%
+      filter(!is.na(drug_type)) %>%
       filter(drug_type == drug)
-    log_reg <- glm(positive_for_drug ~ year, data = alcohol,
-                   family = binomial(link = "logit"))
-    x <- summary(log_reg)$coefficients 
-    x = as.data.frame(x)
-    x %>% 
-      slice(2) %>% 
-      select(3:4)
   }
+  log_reg <- glm(positive_for_drug ~ year, data = to_test,
+                 family = binomial(link = "logit"))
+  log_reg_clean <- slice(tidy(log_reg), 2)
+  Z <- log_reg_clean$statistic
+  p.value <- log_reg_clean$p.value
+  
+  Z <- round(Z, digits = 1)
+  p.value <- round(p.value, digits = 3)
+  
+  log_reg_table <- tibble::tibble(Z, p.value)
+  
+  log_reg_trend <- log_reg_table %>% 
+    mutate(Z = as.numeric(Z)) %>% 
+    mutate(p.value = as.numeric(p.value))
+  
+  return(log_reg_trend)
 }
